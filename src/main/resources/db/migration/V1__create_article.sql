6cbb9f1767b0c8a73475e9bc227a2d3f36e069a9
gitaepark97 <parkkitae7@gmail.com>
2025-05-26 15:01:17 +0900

    feat: article 테이블 설계

---
+++ src/main/resources/db/migration/V1__create_article.sql
@@ -0,0 +1,10 @@
+CREATE TABLE article
+(
+    article_id bigint        not null primary key,
+    title      varchar(100)  not null,
+    content    varchar(3000) not null,
+    board_id   bigint        not null,
+    writer_id  bigint        not null,
+    created_at datetime      not null,
+    updated_at datetime      not null
+);
