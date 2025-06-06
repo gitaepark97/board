34a0cf63f6b81bc8b4b039123e5c19c7c51ecfec
gitaepark97 <parkkitae7@gmail.com>
2025-06-06 18:03:23 +0900

    fix: 게시글 조회 API
    조회 수 증가 Transactional 추가

--- src/main/java/board/backend/application/ArticleViewWriter.java
+++ src/main/java/board/backend/application/ArticleViewWriter.java
@@ -2,6 +2,7 @@ package board.backend.application;
 
 import board.backend.domain.ArticleViewCount;
 import board.backend.infra.ArticleViewCountRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +12,15 @@ class ArticleViewWriter {
 
     private final ArticleViewCountRepository articleViewCountRepository;
 
+    @Transactional
     void increase(Long articleId) {
         long result = articleViewCountRepository.increase(articleId);
         if (result == 0) {
             articleViewCountRepository.save(ArticleViewCount.init(articleId));
         }
     }
-    
+
+    @Transactional
     void deleteArticle(Long articleId) {
         articleViewCountRepository.deleteById(articleId);
     }
